3edc4105b8bbcd6e0ce3b77261ff91da7b99f43a
dtou-masa <s1280111@u-aizu.ac.jp>
2021-07-08 14:45:28 +0900

    Won/lost message added

--- dicegame.c
+++ dicegame.c
@@ -5,10 +5,26 @@
 int main()
 {
   int die1 = 0, die2 = 0;
+  char name[20];
+
+  printf("What is your name\n>");
+  scanf("%s",name);
+  printf("Hello, %s!\n", name);
   printf("Rolling the dice...\n");
   srand((unsigned)time(NULL));
   die1 = rand() % 6 + 1;
   die2 = rand() % 6 + 1;
   printf("Die 1:%d\nDie 2:%d\n",die1,die2);
   printf("Total value: %d\n", die1 + die2);
+  
+   if(die1 + die2 > 7)
+    {
+      printf("You won\n");
+    }
+  else if(die1 + die2 <= 7)
+    {
+      printf("You lost\n");
+    }
+  return 0;
 }
+
